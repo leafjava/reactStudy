b358dc75a2d70d2d05fc790cbed5ce2e79b21a3d
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-08 10:19:50 +0800

    1564_day87_Redux核心原理-React路由-Hooks-03_(了解)Redux-实现applyMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { configureStore,applyMiddleware,compose,combineReducers } from '@reduxjs/toolkit'
+import { configureStore,compose,combineReducers } from '@reduxjs/toolkit'
 // import thunk from "redux-thunk"
 import counterReducer from './counter'
 import homeReducer from './home'
 import userReducer from './user'
+
+import {log,thunk,applyMiddleware} from './middleware'
 
 // 正常情况下 store.dispatch(对象)
 // 想要派发函数 store.dispatch(function)
@@ -44,35 +46,35 @@ const store = configureStore({
   reducer
 })
 
-// 对每次派发的action进行拦截，进行日志打印
-function log(store){
-  const next = store.dispatch
+// // 对每次派发的action进行拦截，进行日志打印
+// function log(store){
+//   const next = store.dispatch
 
-  function logAndDispatch(action){
-    console.log("当前派发的action:",action)
+//   function logAndDispatch(action){
+//     console.log("当前派发的action:",action)
 
-    // 真正派发的代码:要使用之前的dispatch进行派发
-    next(action)
+//     // 真正派发的代码:要使用之前的dispatch进行派发
+//     next(action)
 
-    console.log("派发之后的结果:",store.getState())
-  }
+//     console.log("派发之后的结果:",store.getState())
+//   }
 
-  // monkey patch:猴补丁 => 篡改现有的代码,对整体的执行逻辑进行修改
-  store.dispatch = logAndDispatch
-}
-log(store)
+//   // monkey patch:猴补丁 => 篡改现有的代码,对整体的执行逻辑进行修改
+//   store.dispatch = logAndDispatch
+// }
+// log(store)
 
-function thunk(store){
-  const next = store.dispatch
-  function dispatchThunk(action){
-    if(typeof action === "function"){
-      action(store.dispatch,store.getState)
-    } else {
-      next(action)
-    }
-  }
-  store.dispatch = dispatchThunk
-}
+// function thunk(store){
+//   const next = store.dispatch
+//   function dispatchThunk(action){
+//     if(typeof action === "function"){
+//       action(store.dispatch,store.getState)
+//     } else {
+//       next(action)
+//     }
+//   }
+//   store.dispatch = dispatchThunk
+// }
 
 // store.dispatch(function(dispatch,getState){
 //   dispatch({
@@ -80,6 +82,8 @@ function thunk(store){
 //   })
 // })
 
-thunk(store)
+// thunk(store)
+
+applyMiddleware(store,log,thunk)
 
 export default store
---
+++ src/store/middleware/index.js
@@ -0,0 +1,9 @@
+import log from './log'
+import thunk from './thunk'
+import applyMiddleware from './appleMiddleware'
+
+export {
+  log,
+  thunk,
+  applyMiddleware
+}
